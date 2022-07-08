519d45bd1351402241b33afdd0c8c474dac8d40c
Ph0-ever <oluwayomiforever@gmail.com>
2022-07-08 06:24:15 +0100

    lesson-141

---
+++ Linked-list-cycle-141/index.js
@@ -0,0 +1,12 @@
+var hasCycle = function (head) {
+    fast = head.next;
+    slow = head;
+    while (fast && fast.next && slow) {
+        if (fast == slow) {
+            return true;
+        }
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return false;
+};
