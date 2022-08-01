526e7dd1a91a8079ad1b60303485ff4a9933635a
Ph0-ever <oluwayomiforever@gmail.com>
2022-08-01 13:43:35 +0100

    lesson-26

---
+++ Remove-Duplicates-26/index.js
@@ -0,0 +1,11 @@
+
+var removeDuplicates = function (nums) {
+    l = 0
+    for (let r = 0; r < nums.length; r++) {
+        if (nums[r] != nums[r - 1]) {
+            nums[l] = nums[r];
+            l++;
+        }
+    }
+    return l;
+};
